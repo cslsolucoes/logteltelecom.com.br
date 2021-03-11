529807d6b5e88472e28d4b1fb0600751b163085a
gabrielymendes19 <gabrielymendesmontes@gmail.com>
2021-03-11 14:50:55 -0300

    barra de conversao

--- views/barraConversao.php
+++ views/barraConversao.php
@@ -22,44 +22,24 @@
         </div>
     </div>
   </div>
-<!-- 
+  
+
   <div class="sz-chat ">
     <spam>Posso Ajudar?</spam>
   </div>
- -->
-<!-- 
-  <div class="bg-primary barra-conversao active d-none d-xl-block">
+ 
+ <div class="bg-primary barra-conversao active  d-xl-none">
     <div class="container">
-          
-        <div class="form-row">
-          <div class="col-md-3 ">
-            <img class="w-50" src="<?= $uri ?>/node_modules/img/logo/logo-branca-oficial.png" alt="First slide">
-          </div> 
-          <div class="col-md-3 mt-1">
-            <a href="<?= $uri ?>/#nossosPlanos" class="col btn btn-light" >
-              <spam class=""> Escolha seu  </spam>
-              <br>
-              <spam style="font-size:16px"> COMBO </spam>
-            </a>
-          </div>   
-          <div class="col-md-3 m-1">
-            <a href="tel:40639001" class="col btn btn-light" >
-               <i class="fa fa-phone cyan-gradient fa-2x "></i> 
-              <spam class="ml-4">Suporte ao cliente </spam>
-              <br>
-              <spam style="font-size:16px"> 61 4063-9001 </spam>
-            </a>
-          </div>    
-          <div class="col-md-3 m-1">
-            <a href="https://api.whatsapp.com/send?phone=5561981956666&text=Ol%C3%A1%2C%20gostaria%20de%20Contratar%20um%20Combo%20da%20Logtel.%20Poderia%20me%20informar%20como%20fa%C3%A7o%3F" class="col btn btn-light">
-               <i class="fa fa-whatsapp green-gradient fa-2x"></i>  
-              <spam class="ml-4">WhatsApp de Vendas </spam>
-              <br>
-              <spam style="font-size:16px"> 61 98195-6666 </spam>
-            </a>
-          </div>  
-        </div>
+      <div class="form-row text-center">
+        <a class="col col-4" href="https://api.whatsapp.com/send?phone=5561981956666&text=Ol%C3%A1%2C%20gostaria%20de%20Contratar%20um%20Combo%20da%20Logtel.%20Poderia%20me%20informar%20como%20fa%C3%A7o%3F" >
+          <i class="fa fa-whatsapp green-gradient fa-2x"></i> <br>
+          <spam class="text-white  ligue-wpp" style="font-size:12px"> whatsApp </spam>
+        </a>
+        <a class="col col-4 btn btn-conversao"  href="<?= $uri ?>/#solicitar-contato"> Solicitar <br> contato </a>
+        <a class="xol col-4" href="https://api.whatsapp.com/send?phone=5561981956666&text=Ol%C3%A1%2C%20gostaria%20de%20Contratar%20um%20Combo%20da%20Logtel.%20Poderia%20me%20informar%20como%20fa%C3%A7o%3F" >
+          <i class="fa fa-whatsapp green-gradient fa-2x"></i> <br>
+          <spam class="text-white  ligue-wpp" style="font-size:12px">Ligar</spam>
+        </a>
+      </div>
     </div>
   </div>
-
-   -->
